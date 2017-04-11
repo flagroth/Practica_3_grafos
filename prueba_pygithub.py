9e94c30f52e79bf1ff7f3e83b0df8f2293b47043
flagroth <javier.cano.montero@urjc.es>
2017-04-11 10:58:20 +0200

    simple_index working

--- prueba_pygithub.py
+++ prueba_pygithub.py
@@ -1,36 +1,48 @@
 from github import Github
 import networkx as nx
 from networkx_viewer import Viewer
-    
+from networkx.readwrite import json_graph
+import json
 
 
-def get_followers(user, followers, visited, pending):
+def get_followers(followers, visited, pending):
+    user=pending.pop(0)
+    print(user)
     visited.append(user)      
     if len(visited)>20:
         return
     try:
         for f in g.get_user(user).get_followers():
             followers.append((user,f.login))
-            print(f.login)
+#            print(f.login)
             if f.login not in visited:
                 pending.append(f.login)
     except :
         return -1
-    user=pending.pop(0)
-    get_followers(user, followers, visited, pending)
+    get_followers(followers, visited, pending)
     
 
-# First create a Github instance:
-    
-if	__name__	==	"__main__":
-    g = Github("flagroth","")    
+if	__name__ == "__main__":
+    # First create a Github instance:
+    g = Github("flagroth","")
     visited=[]
-    pending=[]
+    pending=["adamtheturtle"]
     followers=[]
-    get_followers("adamtheturtle", followers, visited, pending)
+    get_followers(followers, visited, pending)
     graph = nx.DiGraph()
     graph.add_edges_from(followers)
-    app = Viewer(graph)
-    app.mainloop()    
-    
+
+    # print(graph.edges())
+    # print(graph.nodes())
+
+    data = json_graph.node_link_data(graph)
+
+    with open('data.json', 'w') as outfile:
+        json.dump(data, outfile)
+        outfile.flush()
+        outfile.close()
+
+    # app = Viewer(graph)
+    # app.mainloop()
+    #
     
